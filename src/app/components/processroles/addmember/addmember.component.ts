554fd506fa07aed8ffd52aee183950d164adc7a1
GamalatSrour <gamalatelsayed97@gmail.com>
2022-02-03 12:00:32 +0200

    memberExport

--- src/app/components/processroles/addmember/addmember.component.ts
+++ src/app/components/processroles/addmember/addmember.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as FileSaver from 'file-saver';
 import { Processsrole } from 'src/app/interfaces/processsrole';
 import { ProcessroleService } from 'src/app/services/processrole.service';
 declare var $: any
@@ -106,6 +107,23 @@ export class AddmemberComponent implements OnInit {
           }
     });
 
+  }
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.customers);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "members");
+    });
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+  let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  let EXCEL_EXTENSION = '.xlsx';
+  const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+  });
+  FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 
 }
